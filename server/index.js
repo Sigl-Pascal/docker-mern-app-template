bb432cd3f4283aaa3a4378856ab5bfeb3dfe3b26
Pascal Sigl <pascal@fam-sigl.de>
2022-02-20 18:41:18 +0100

    Added MongoDB Connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World');
 })
+
+const db = 'mongodb://mongo:27017';
+mongoose.connect(db)
+  .then(() => {console.log('MongoDB connected...')})
+  .catch(err => console.log(err));
 
 app.listen(5000)
